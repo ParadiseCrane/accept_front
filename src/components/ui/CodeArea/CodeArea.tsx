42c344e8911fac7b6a24c82af931f528571bf433
melaveetha <retromeras@ya.ru>
2023-08-01 20:31:58 +0300

    Improve [hotkeys] CodeArea

--- src/components/ui/CodeArea/CodeArea.tsx
+++ src/components/ui/CodeArea/CodeArea.tsx
@@ -1,4 +1,11 @@
-import { FC, ReactNode, memo, useCallback, useRef } from 'react';
+import {
+  FC,
+  ReactNode,
+  RefObject,
+  memo,
+  useCallback,
+  useRef,
+} from 'react';
 import { useLocale } from '@hooks/useLocale';
 import styles from './codeArea.module.css';
 import { callback } from '@custom-types/ui/atomic';
@@ -12,6 +19,59 @@ import { extensionValidator } from '@utils/extensionValidator';
 import { Dropzone, TextArea } from '@ui/basics';
 import { MyButtonProps } from '@custom-types/ui/basics/button';
 import { getHotkeyHandler } from '@mantine/hooks';
+
+const get_selection = (ref: HTMLTextAreaElement) => {
+  let start = ref.selectionStart;
+  let end = ref.selectionEnd;
+  return [start, end];
+};
+
+const find_selected_lines = (
+  value: string,
+  start: number,
+  end: number
+) => {
+  value = value.trimEnd() + '\n'; // for last line to work properly in case of not having \n at the end
+  let rows = value.split('\n');
+  let acc_len = 0;
+  let row_bounds = [];
+  let prev_index = 0;
+  let next_index = 0;
+  for (let index = 0; index < rows.length; index++) {
+    let row_start = acc_len;
+    let row_end = acc_len + rows[index].length + 1;
+
+    if (row_start <= start && start < row_end) {
+      prev_index = index - 1;
+      if (start === end) {
+        // special case when cursor placed in begining of line
+        end = row_end;
+      }
+      start = row_start;
+    }
+    if (row_start < end && end <= row_end) {
+      next_index = index + 1;
+      end = row_end;
+    }
+
+    row_bounds.push([row_start, row_end]);
+    acc_len += rows[index].length + 1; // +1 is \n
+  }
+
+  let prev_row = [0, 0];
+  if (prev_index >= 0) prev_row = row_bounds[prev_index];
+  let next_row = [value.length, value.length];
+  if (next_index < row_bounds.length)
+    next_row = row_bounds[next_index];
+
+  let begining = value.substring(0, prev_row[0]);
+  let prev = value.substring(prev_row[0], prev_row[1]);
+  let selected = value.substring(start, end);
+  let next = value.substring(next_row[0], next_row[1]);
+  let ending = value.substring(next_row[1]);
+
+  return [begining, prev, selected, next, ending];
+};
 
 const CodeArea: FC<{
   label: string;
@@ -41,12 +101,10 @@ const CodeArea: FC<{
   const { locale } = useLocale();
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
-  const process_tab_keydown = useCallback(() => {
-    if (!textAreaRef.current) return;
+  const tab = useCallback(() => {
     let ref = textAreaRef.current;
     if (!ref) return;
-    let start = ref.selectionStart;
-    let end = ref.selectionEnd;
+    let [start, end] = get_selection(ref);
 
     let value = ref.value;
     ref.value = `${value.substring(0, start)}\t${value.substring(
@@ -56,9 +114,63 @@ const CodeArea: FC<{
     ref.selectionEnd = start + 1;
   }, [textAreaRef]);
 
-  const process_send_key_down = useCallback(() => {
+  const send_keyboard = useCallback(() => {
     if (onSend) onSend();
   }, [onSend]);
+
+  const brackets = useCallback(
+    (open_bracket: string, close_bracket: string) => {
+      return () => {
+        let ref = textAreaRef.current;
+        if (!ref) return;
+        let [start, end] = get_selection(ref);
+
+        let value = ref.value;
+        ref.value = `${value.substring(
+          0,
+          start
+        )}${open_bracket}${value.substring(
+          start,
+          end
+        )}${close_bracket}${value.substring(end)}`;
+        ref.selectionStart = start + 1;
+        ref.selectionEnd = end + 1;
+      };
+    },
+    [textAreaRef]
+  );
+
+  const row_swap_up = useCallback(() => {
+    let ref = textAreaRef.current;
+    if (!ref) return;
+    let [start, end] = get_selection(ref);
+
+    let result = find_selected_lines(ref.value, start, end);
+    if (!result) return;
+
+    let [begining, prev, selected, next, ending] = result;
+
+    ref.value = `${begining}${selected}${prev}${next}${ending}`;
+
+    ref.selectionStart = start - prev.length;
+    ref.selectionEnd = end - prev.length;
+  }, [textAreaRef]);
+
+  const row_swap_down = useCallback(() => {
+    let ref = textAreaRef.current;
+    if (!ref) return;
+    let [start, end] = get_selection(ref);
+
+    let result = find_selected_lines(ref.value, start, end);
+    if (!result) return;
+
+    let [begining, prev, selected, next, ending] = result;
+
+    ref.value = `${begining}${prev}${next}${selected}${ending}`;
+
+    ref.selectionStart = start + next.length;
+    ref.selectionEnd = end + next.length;
+  }, [textAreaRef]);
 
   const onDrop = useCallback(
     (files: File[]) => {
@@ -121,8 +233,13 @@ const CodeArea: FC<{
             onChange={(e) => setCode(e.target.value)}
             minRows={minRows || 20}
             onKeyDown={getHotkeyHandler([
-              ['Tab', process_tab_keydown],
-              ['mod+Enter', process_send_key_down],
+              ['Tab', tab],
+              ['mod+Enter', send_keyboard],
+              ['shift+BracketLeft', brackets('{', '}')],
+              ['shift+Digit9', brackets('(', ')')],
+              ['BracketLeft', brackets('[', ']')],
+              ['alt+ArrowUp', row_swap_up],
+              ['alt+ArrowDown', row_swap_down],
             ])}
             {...formProps}
           />
