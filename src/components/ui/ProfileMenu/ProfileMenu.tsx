494d5bef1ef836e9a8a99942028f20c1dddc1cdd
melaveetha <retromeras@ya.ru>
2024-09-18 20:56:30 +0300

    Draft [add accounts] ui/ProfileMenu

--- src/components/ui/ProfileMenu/ProfileMenu.tsx
+++ src/components/ui/ProfileMenu/ProfileMenu.tsx
@@ -10,7 +10,7 @@ import {
 import { useBackNotifications } from '@hooks/useBackNotifications';
 import { Indicator, UserAvatar } from '@ui/basics';
 import styles from './profileMenu.module.css';
-import { Logout } from 'tabler-icons-react';
+import { Logout, Plus } from 'tabler-icons-react';
 import { accessLevels } from '@constants/protectedRoutes';
 import { menuLinks } from '@constants/ProfileMenuLinks';
 import Link from 'next/link';
@@ -44,82 +44,111 @@ const ProfileMenu: FC<{}> = ({}) => {
   }, [locale, signOut]);
 
   return (
-    <Menu
-      trigger="click"
-      zIndex={1000}
-      transitionProps={{ transition: 'scale-y', duration: 150 }}
-    >
-      <Menu.Target>
-        <div>
-          {/* TODO check is div needed */}
-          <Indicator label={unviewed} disabled={unviewed <= 0}>
-            <UserAvatar
-              login={user?.login}
-              alt={'Users avatar'}
-              classNames={{ root: styles.avatar }}
-            />
-          </Indicator>
-        </div>
-      </Menu.Target>
-      <Menu.Dropdown>
-        <Menu.Label className={styles.label}>
-          {user?.shortName || ''}
-        </Menu.Label>
-        {accounts.length > 0 && (
-          <>
-            <Menu.Divider />
-            <Menu.Label className={styles.label}>
-              {'Аккаунты'}
-            </Menu.Label>
+    <div className={styles.wrapper}>
+      <Menu
+        trigger="click"
+        zIndex={1000}
+        transitionProps={{ transition: 'scale-y', duration: 150 }}
+      >
+        <Menu.Target>
+          <div>
+            {/* TODO check is div needed */}
+            <Indicator label={unviewed} disabled={unviewed <= 0}>
+              <UserAvatar
+                login={user?.login}
+                alt={'Users avatar'}
+                classNames={{ root: styles.avatar }}
+              />
+            </Indicator>
+          </div>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Label className={styles.label}>
+            {user?.shortName || ''}
+          </Menu.Label>
 
-            {accounts.map((item, index) => (
+          <Menu.Divider />
+          <Menu.Label className={styles.label}>
+            {'Полезное'}
+          </Menu.Label>
+
+          {menuLinks
+            .filter(
+              (item) =>
+                !item.permission ||
+                accessLevel >= accessLevels[item.permission]
+            )
+            .map((item, index) => (
               <Menu.Item
+                component={Link}
+                href={item.href}
                 key={index}
-                icon={
-                  <UserAvatar
-                    radius="md"
-                    size="md"
-                    login={item.login}
-                    alt={'Users avatar'}
-                  />
-                }
+                icon={item.icon}
               >
-                {item.organization}
+                {item.text(locale)}
               </Menu.Item>
             ))}
-          </>
-        )}
 
-        <Menu.Divider />
-        <Menu.Label className={styles.label}>{'Полезное'}</Menu.Label>
+          {accounts.length > 0 && (
+            <>
+              <Menu.Divider />
+              <Menu.Label className={styles.label}>
+                {'Аккаунты'}
+              </Menu.Label>
 
-        {menuLinks
-          .filter(
-            (item) =>
-              !item.permission ||
-              accessLevel >= accessLevels[item.permission]
-          )
+              {accounts.map((item, index) => (
+                <Menu.Item
+                  key={index}
+                  icon={
+                    <UserAvatar
+                      radius="md"
+                      size="md"
+                      login={item.login}
+                      alt={'Users avatar'}
+                    />
+                  }
+                >
+                  {item.organization}
+                </Menu.Item>
+              ))}
+            </>
+          )}
+
+          <Menu.Divider />
+          <Menu.Label className={styles.label}>
+            {'Аккаунт'}
+          </Menu.Label>
+
+          <Menu.Item
+            onClick={handleSignOut}
+            icon={<Plus color="var(--secondary)" size={20} />}
+          >
+            {'Добавить аккаунт'}
+          </Menu.Item>
+
+          <Menu.Item
+            onClick={handleSignOut}
+            icon={<Logout color="var(--secondary)" size={20} />}
+          >
+            {locale.mainHeaderLinks.profileLinks.signOut}
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+      {accounts.length > 0 &&
+        accounts
+          .slice(0, Math.min(2, accounts.length))
           .map((item, index) => (
-            <Menu.Item
-              component={Link}
-              href={item.href}
+            <UserAvatar
+              style={{
+                marginLeft: '-5px',
+                outline: '1px solid white',
+              }}
               key={index}
-              icon={item.icon}
-            >
-              {item.text(locale)}
-            </Menu.Item>
+              login={item.login}
+              size="sm"
+            />
           ))}
-        <Menu.Divider />
-        <Menu.Label className={styles.label}>{'Аккаунт'}</Menu.Label>
-
-        <Menu.Item
-          onClick={handleSignOut}
-          icon={<Logout color="var(--secondary)" size={20} />}
-        >
-          {locale.mainHeaderLinks.profileLinks.signOut}
-        </Menu.Item>
-      </Menu.Dropdown>
-    </Menu>
+    </div>
   );
 };
 
