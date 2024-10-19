4da0538929423e8b56ead4b1789e2397c048ac1b
melaveetha <retromeras@ya.ru>
2024-10-19 23:50:27 +0300

    Fix [secure cookies] createTokenCookie.ts

--- src/utils/createTokenCookie.ts
+++ src/utils/createTokenCookie.ts
@@ -1,13 +1,10 @@
 import { serialize } from 'cookie';
 
-export const createTokenCookie = (
-  name: string,
-  value: string,
-  expires: any
-) =>
+export const createTokenCookie = (name: string, value: string, expires: any) =>
   serialize(name, value, {
     expires,
-    secure: process.env.NODE_ENV !== 'development',
+    // secure: process.env.NODE_ENV !== 'development',
+    secure: false,
     path: '/',
     sameSite: 'strict',
   });
