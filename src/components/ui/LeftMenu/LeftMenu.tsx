c0c1e4482c2a8911d3c21ecf911e96b86eebeb36
melaveetha <retromeras@ya.ru>
2024-10-30 12:48:35 +0300

    Fix [move to new component] components/LeftMenu

--- src/components/ui/LeftMenu/LeftMenu.tsx
+++ src/components/ui/LeftMenu/LeftMenu.tsx
@@ -1,5 +1,5 @@
 import { IMenuLink } from '@custom-types/ui/IMenuLink';
-import { AppShell, Group, UnstyledButton } from '@mantine/core';
+import { Box, Group, NavLink, UnstyledButton } from '@mantine/core';
 import { FC, ReactNode, memo, useState } from 'react';
 import styles from './leftMenu.module.css';
 
@@ -11,39 +11,25 @@ const LeftMenu: FC<{
   const [current, setCurrent] = useState(initialStep || 0);
 
   return (
-    <AppShell>
-      <AppShell.Navbar p="xs" w={{ base: 300 }} withBorder={false} zIndex={10}>
-        {topContent && <></>}
-        {/* <Navbar.Section grow mt="md"> */}
-        {links.map((element, idx) => (
-          <UnstyledButton
-            key={idx}
-            className={`${styles.button} ${
-              current === idx ? styles.activeButton : ''
-            }`}
-            onClick={() => setCurrent(idx)}
-          >
-            <Group gap="xs" className={styles.groupWrapper}>
-              <div
-                className={`${styles.line} ${
-                  current === idx ? styles.activeLine : ''
-                }`}
-              ></div>
-              <Group className={styles.groupWrapper}>
-                {element.icon}
-                <div>{element.title}</div>
-              </Group>
-            </Group>
-          </UnstyledButton>
-        ))}
-        {/* </Navbar.Section> */}
-      </AppShell.Navbar>
-      <AppShell.Main>
-        <div className={styles.pageWrapper}>
-          {links[current]?.page || links[0]?.page || ''}
-        </div>
-      </AppShell.Main>
-    </AppShell>
+    <div className={styles.wrapper}>
+      <Box p="xs" w={300}>
+        {topContent && <>{topContent}</>}
+        <>
+          {links.map((element, idx) => (
+            <NavLink
+              key={idx}
+              active={idx == current}
+              onClick={() => setCurrent(idx)}
+              label={element.title}
+              leftSection={element.icon}
+            />
+          ))}
+        </>
+      </Box>
+      <div className={styles.pageWrapper}>
+        {links[current]?.page || links[0]?.page || ''}
+      </div>
+    </div>
   );
 };
 
