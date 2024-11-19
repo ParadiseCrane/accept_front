4e93c63dac7fa247f5e815137af2a0058c3f801f
NikitaKalitov <162014214+NikitaKalitov@users.noreply.github.com>
2024-11-19 23:18:43 +0300

    working with links

--- src/components/ui/basics/TipTapEditor/Components/LinkButton.tsx
+++ src/components/ui/basics/TipTapEditor/Components/LinkButton.tsx
@@ -1,23 +1,41 @@
 import { RichTextEditor } from '@mantine/tiptap';
 import { Editor } from '@tiptap/react';
-import { Link as LinkIcon } from 'tabler-icons-react';
+import { Link as LinkIcon, Unlink as UnlinkIcon } from 'tabler-icons-react';
 
 export const LinkButton = ({ editor }: { editor: Editor }) => {
   const isActive = editor.isFocused ? editor.isActive('link') : false;
   return (
     <RichTextEditor.Control
       onClick={() => {
-        editor
-          .chain()
-          .setLink({
-            href: 'https://upload.wikimedia.org/wikipedia/commons/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg',
-          })
-          .run();
+        if (isActive) {
+          console.log('link is', editor.getAttributes('link')['href']);
+        } else {
+          editor
+            .chain()
+            .setLink({
+              href: 'https://upload.wikimedia.org/wikipedia/commons/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg',
+            })
+            .run();
+        }
       }}
       aria-label="Set link"
       title="Set link"
     >
       <LinkIcon style={isActive ? { stroke: 'red' } : {}} size={'1rem'} />
+    </RichTextEditor.Control>
+  );
+};
+
+export const UnlinkButton = ({ editor }: { editor: Editor }) => {
+  return (
+    <RichTextEditor.Control
+      onClick={() => {
+        editor.chain().unsetLink().run();
+      }}
+      aria-label="Remove link"
+      title="Remove link"
+    >
+      <UnlinkIcon size={'1rem'} />
     </RichTextEditor.Control>
   );
 };
