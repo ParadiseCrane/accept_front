9affa5c641363cd3e54c6df9d3b5599d76f50984
Dmitry <pro100pro10010@gmail.com>
2024-12-06 17:22:20 +0300

    Add [legacy redirect] layouts

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,15 +1,37 @@
-import { FC, ReactNode } from 'react';
+'use client';
+
+import { FC, ReactNode, useEffect, useState } from 'react';
 import Navbar from '@components/layout/Navbar/Navbar';
 import Footer from '@components/layout/Footer/Footer';
+import { getCookie } from 'cookies-next';
+import { LegacyRedirect } from './LegacyRedirect';
 
 export const DefaultLayout: FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  return (
+  const [isMounted, setIsMounted] = useState(false);
+
+  const [skipRedirect, _] = useState(
+    getCookie('skip')?.toString() ?? ''
+  );
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return isMounted ? (
     <>
-      <Navbar />
-      <main style={{ minHeight: '100vh' }}>{children}</main>
-      <Footer />
+      {skipRedirect ? (
+        <>
+          <Navbar />
+          <main style={{ minHeight: '100vh' }}>{children}</main>
+          <Footer />
+        </>
+      ) : (
+        <LegacyRedirect />
+      )}
     </>
+  ) : (
+    <></>
   );
 };
---
+++ src/layouts/LegacyRedirect.tsx
@@ -0,0 +1,60 @@
+'use client';
+import styles from '@styles/error.module.css';
+
+import { FC } from 'react';
+
+import Title from '@ui/Title/Title';
+import Link from 'next/link';
+
+export const LegacyRedirect: FC<{}> = ({}) => {
+  return (
+    <div className={styles.wrapper}>
+      <Title title={'Переезд!'} />
+      <div style={{ fontSize: '10rem', textAlign: 'center' }}>
+        {'🏎️ᯓᡣ𐭩'}
+      </div>
+      <div
+        style={{
+          textAlign: 'center',
+          fontSize: '2.2rem',
+          marginTop: '-3rem',
+        }}
+      >
+        <div>Accept переехал на новый адрес!</div>
+        <div>
+          Переходите по ссылке ниже и продолжайте пользоваться нашей
+          системой!
+        </div>
+      </div>
+      <Link href="https://код.ацепт.рф/" className={styles.return}>
+        Перейти на новый сайт!
+      </Link>
+      <div
+        style={{
+          textAlign: 'center',
+          fontSize: '1.5rem',
+          display: 'flex',
+          gap: '0.5rem',
+          alignItems: 'center',
+        }}
+      >
+        <div>P.S. А ещё у нас есть </div>
+        <Link
+          style={{
+            textDecoration: 'unset',
+            color: 'var(--primary)',
+            backgroundColor: 'white',
+            borderRadius: '5px',
+            padding: 'var(--spacer-xs) var(--spacer-s)',
+          }}
+          href="https://ацепт.рф/"
+        >
+          Лендинг
+        </Link>
+      </div>
+      <div style={{ color: 'var(--primary)' }}>
+        чтобы продолжить на этом сайте, добавьте в куках "skip"="1"
+      </div>
+    </div>
+  );
+};
