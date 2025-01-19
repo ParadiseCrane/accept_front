11b7ed46768f100d20537efe886f42c8a4dc5cb7
Dmitry <pro100pro10010@gmail.com>
2025-01-19 12:40:01 +0300

    Fix [can ban request] attempt page

--- pages/api/rights/index.ts
+++ pages/api/rights/index.ts
@@ -1,13 +1,14 @@
 import { fetchWrapper } from '@utils/fetchWrapper';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function AttemptCanBan(
+export default async function Rights(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   await fetchWrapper({
     req: req,
     res: res,
-    url: `api/rights/write/${req.query.spec}`,
+    url: 'api/rights',
+    method: 'POST',
   });
 }
