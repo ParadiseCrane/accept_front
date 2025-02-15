7a6fb2c38ded38d5aad37e90af1674b8b225dd2c
NikitaKalitov <162014214+NikitaKalitov@users.noreply.github.com>
2025-02-15 23:53:18 +0300

    Fix [EmptyTablePlaceholder]

--- src/components/ui/basics/EmptyTablePlaceholder/EmptyTablePlaceholder.tsx
+++ src/components/ui/basics/EmptyTablePlaceholder/EmptyTablePlaceholder.tsx
@@ -1,94 +1,37 @@
 import { DEFAULT_ON_PAGE } from '@constants/Defaults';
-import { IRole } from '@custom-types/data/atomic';
-import { IUser, IUserDisplay } from '@custom-types/data/IUser';
 import { BaseSearch } from '@custom-types/data/request';
 import { ILocale } from '@custom-types/ui/ILocale';
 import { ITableColumn } from '@custom-types/ui/ITable';
 import { useLocale } from '@hooks/useLocale';
-import { useRequest } from '@hooks/useRequest';
 import tableStyles from '@styles/ui/customTable.module.css';
 import Table from '@ui/Table/Table';
-import { customTableSort } from '@utils/customTableSort';
-import Fuse from 'fuse.js';
-import Link from 'next/link';
-import {
-  FC,
-  ReactNode,
-  memo,
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react';
+import { FC, ReactNode, memo, useMemo, useState } from 'react';
 import styles from './style.module.css';
-import { IUserDisplayItem } from '@ui/SimpleUserList/SimpleUserList';
 
-const initialUsers: IUserDisplay[] = [
-  {
-    role: {
-      spec: 12,
-      name: 'role 1',
-      accessLevel: 1,
-    },
-    login: '',
-    shortName: 'shortname 1',
-  },
-  {
-    role: {
-      spec: 12,
-      name: 'role 1',
-      accessLevel: 1,
-    },
-    login: '',
-    shortName: 'shortname 2',
-  },
-  {
-    role: {
-      spec: 12,
-      name: 'role 1',
-      accessLevel: 1,
-    },
-    login: '',
-    shortName: 'shortname 3',
-  },
-  {
-    role: {
-      spec: 121,
-      name: 'role 2',
-      accessLevel: 2,
-    },
-    login: '',
-    shortName: 'shortname 11',
-  },
-  {
-    role: {
-      spec: 121,
-      name: 'role 2',
-      accessLevel: 2,
-    },
-    login: '',
-    shortName: 'shortname 21',
-  },
-  {
-    role: {
-      spec: 121,
-      name: 'role 2',
-      accessLevel: 2,
-    },
-    login: '',
-    shortName: 'shortname 31',
-  },
-];
-
-interface Item<T = any> {
-  value: T;
-  display: string | ReactNode;
+interface IEmptyItem {
+  content: {
+    value: string;
+    display: ReactNode;
+  };
 }
+
+const fillList = (numOfRows: number): IEmptyItem[] => {
+  const list: IEmptyItem[] = [];
+  for (let i = 0; i < numOfRows; i++) {
+    list.push({
+      content: {
+        value: '',
+        display: <div className={tableStyles.titleWrapper} />,
+      },
+    });
+  }
+  return list;
+};
 
 const initialColumns = (locale: ILocale): ITableColumn[] => [
   {
     label: locale.ui.table.emptyTableTitle,
-    key: 'login',
+    key: 'content',
     sortable: true,
     sortFunction: (a: any, b: any) =>
       a.login.value > b.login.value
@@ -104,30 +47,6 @@ const initialColumns = (locale: ILocale): ITableColumn[] => [
   },
 ];
 
-const refactorUser = (user: IUserDisplay): any => ({
-  ...user,
-  login: {
-    value: user.login,
-    display: <div className={tableStyles.titleWrapper}>{user.login}</div>,
-  },
-  shortName: {
-    value: user.shortName,
-    display: user.shortName,
-  },
-  role: {
-    value: user.role,
-    display: (
-      <div
-        style={{
-          color: user.role.accessLevel > 50 ? 'var(--accent)' : 'black',
-        }}
-      >
-        {user.role.name}
-      </div>
-    ),
-  },
-});
-
 const EmptyTablePlaceholder: FC = () => {
   const { locale } = useLocale();
   const defaultOnPage = useMemo(() => DEFAULT_ON_PAGE, []);
@@ -137,15 +56,8 @@ const EmptyTablePlaceholder: FC = () => {
     [initialColumns, locale]
   );
 
-  const [users, setUsers] = useState<IUserDisplayItem[]>([]);
-  const [total, setTotal] = useState(0);
-
-  useEffect(() => {
-    const newList: IUserDisplayItem[] = [];
-    initialUsers.map((user) => newList.push(refactorUser(user)));
-    setUsers(newList);
-    setTotal(newList.length);
-  }, []);
+  const numOfRows = 6;
+  const content: IEmptyItem[] = fillList(numOfRows);
 
   const [searchParams, setSearchParams] = useState<BaseSearch>({
     pager: {
@@ -155,7 +67,7 @@ const EmptyTablePlaceholder: FC = () => {
     sort_by: [],
     search_params: {
       search: '',
-      keys: ['shortName.value', 'login.value'],
+      keys: ['content.value'],
     },
   });
 
@@ -167,7 +79,7 @@ const EmptyTablePlaceholder: FC = () => {
       <div className={styles.tablePadding}>
         <Table
           columns={columns}
-          rows={users}
+          rows={content}
           classNames={{
             wrapper: tableStyles.wrapper,
             table: tableStyles.table,
@@ -182,7 +94,7 @@ const EmptyTablePlaceholder: FC = () => {
           noDefault={true}
           defaultOnPage={defaultOnPage}
           onPage={[5, defaultOnPage]}
-          total={total}
+          total={numOfRows}
           empty={<>{locale.ui.table.emptyMessage}</>}
           isEmpty={false}
           nothingFound={<>{locale.ui.table.nothingFoundMessage}</>}
