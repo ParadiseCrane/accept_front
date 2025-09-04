a9b79b69a71d8b3fe9a95484826d1071722e94c5
NikitaKalitov <nikitakalitov@yandex.ru>
2025-09-04 21:33:48 +0300

    Fix [viewpage styles] CoursePage

--- src/components/Course/Main/Main.tsx
+++ src/components/Course/Main/Main.tsx
@@ -8,6 +8,8 @@ import { TipTapEditor } from '@ui/basics/TipTapEditor/TipTapEditor';
 import Lesson from '../Lesson/Lesson';
 import { useSearchParams } from 'next/navigation';
 
+import styles from './main.module.css';
+
 // TODO mocked method
 const defaultLesson = (lesson: ILesson): ILesson => {
   return {
@@ -31,7 +33,6 @@ const Main: FC = () => {
               ? defaultLesson(res.response)
               : (res.response as ICourse | IUnit | ILesson)
           );
-          // setEntity(res.response);
         }
       );
     }
@@ -40,43 +41,45 @@ const Main: FC = () => {
   if (!entity || !spec || (spec && entity.spec !== spec)) return null;
 
   return (
-    <AppShell.Main>
-      {'tasks' in entity ? (
-        <Lesson lesson={entity} />
-      ) : (
-        <>
-          {entity.kind === 'course' && (
-            <ImageComponent
-              index={0}
-              item={entity.image}
-              active={false}
-              animate
-              height={240}
-              radius="md"
-              imageStyle={{
-                width: '100%',
-                height: 'auto',
-                maxHeight: 240,
-                objectFit: 'cover',
-              }}
-              cover
-            />
-          )}
-          <Center mt={'md'} mb={'md'}>
-            <Title order={1} ta={'center'}>
-              {entity.title}
-            </Title>
-          </Center>
-          <Box ml={'xl'} mr={'xl'}>
-            <TipTapEditor
-              key={entity.spec}
-              editorMode={false}
-              content={entity.description}
-              onUpdate={() => {}}
-            />
-          </Box>
-        </>
-      )}
+    <AppShell.Main classNames={{ main: styles.main }}>
+      <div className={styles.contentWrapper}>
+        {'tasks' in entity ? (
+          <Lesson lesson={entity} />
+        ) : (
+          <>
+            {entity.kind === 'course' && (
+              <ImageComponent
+                index={0}
+                item={entity.image}
+                active={false}
+                animate
+                height={240}
+                radius="md"
+                imageStyle={{
+                  width: '100%',
+                  height: 'auto',
+                  maxHeight: 240,
+                  objectFit: 'cover',
+                }}
+                cover
+              />
+            )}
+            <Center mt={'md'} mb={'md'}>
+              <Title order={1} ta={'center'}>
+                {entity.title}
+              </Title>
+            </Center>
+            <Box ml={'xl'} mr={'xl'}>
+              <TipTapEditor
+                key={entity.spec}
+                editorMode={false}
+                content={entity.description}
+                onUpdate={() => {}}
+              />
+            </Box>
+          </>
+        )}
+      </div>
     </AppShell.Main>
   );
 };
